c35ce0b5aa9038a88e8c09afccd9d10ee50e0906
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-14 00:19:11 -0500

    make toggle button

--- scripts/main.js
+++ scripts/main.js
@@ -8,20 +8,17 @@ function sayOuch() {
  
 // Use a CSS selector to identify an element
 var foxImage = document.querySelector('img');
+var toggle = document.getElementById('toggle')
 
-var appear_button = document.getElementById('appear')
-var disappear_button = document.getElementById('disappear')
-
- 
 // Assign the function to the onclick event on that element
 foxImage.onclick = sayOuch;
 
-disappear_button.onclick = function(){
-	foxImage.style.visibility="hidden";
-}
-appear_button.onclick = function(){
-	foxImage.style.visibility="visible";
+function togg() {
+	if (foxImage.style.visibility=="visible") {
+		foxImage.style.visibility="hidden";
+	} else {
+		foxImage.style.visibility="visible";
+	}
 }
 
-
-// for second bonus: use if statement
+toggle.onclick = togg;
