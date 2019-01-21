009b31620fc77a02af7bff3ae2850ca000afcd7b
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-21 17:55:03 -0500

    back to one button

--- scripts/main.js
+++ scripts/main.js
@@ -8,38 +8,21 @@ function sayOuch() {
  
 // Use a CSS selector to identify an element
 var foxImage = document.querySelector('img');
-// var toggle = document.getElementById('toggle')
-var appear = document.getElementById('appear')
-var disappear = document.getElementById('disappear')
+var toggle = document.getElementById('toggle')
 
 
 
 // Assign the function to the onclick event on that element
 foxImage.onclick = sayOuch;
 
+// back to the one-button version
 
-
-// The two-button version I had before was a little
-// slow--needed extra clicking--so here's a better
-// version
-
-function app() {
-	foxImage.style.display='block'
+function togg() {
+	if (foxImage.style.display=="block") {
+		foxImage.style.display="none";
+	} else {
+		foxImage.style.display="block";
+	}
 }
 
-function disapp() {
-	foxImage.style.display='none'
-}
-
-appear.onclick = app;
-disappear.onclick = disapp;
-
-// function togg() {
-// 	if (foxImage.style.display=="block") {
-// 		foxImage.style.display="none";
-// 	} else {
-// 		foxImage.style.display="block";
-// 	}
-// }
-
-// toggle.onclick = togg;
+toggle.onclick = togg;
